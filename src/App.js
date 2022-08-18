db4c7d7891c5fd9872078ffdd9369d85a0a4b3a4
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-18 23:06:23 +0500

    Navbar and routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
         </Routes>
       </Router>
     </>
