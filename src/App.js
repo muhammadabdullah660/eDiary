750513e62f6296ca3bff7fd0edd2705f0d15a59b
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-18 23:59:46 +0500

    NoteState

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
+import NoteState from "./Context/Notes/NoteState";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- src/Context/Notes/NoteState.js
+++ src/Context/Notes/NoteState.js
@@ -1,4 +1,12 @@
-import React from "react";
-import noteContext from "./noteContext";
+import NoteContext from "./noteContext";
 
-const NoteState = () => {};
+const NoteState = () => {
+  const state = {
+    name: "Abd",
+    class: "25",
+  };
+  return (
+    <NoteContext.provider value={state}>{props.children}</NoteContext.provider>
+  );
+};
+export default NoteState;
