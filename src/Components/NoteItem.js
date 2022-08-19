7efd7ae2d4df50d1ad52e9ea3d4bccb304a12392
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-19 23:49:50 +0500

    refactoring app by creating two more components

---
+++ src/Components/NoteItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoteItem = (props) => {
+  const { note } = props;
+  return (
+    <div>
+      {note.title}
+      {note.description}
+    </div>
+  );
+};
+
+export default NoteItem;
---
+++ src/Components/Notes.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import noteContext from "../Context/Notes/noteContext";
+import NoteItem from "./NoteItem";
+const Notes = () => {
+  const context = useContext(noteContext);
+  const { notes, setnotes } = context;
+  return (
+    <>
+      <div className="container my-3">
+        <h1>Your Notes</h1>
+        {notes.map((note) => {
+          return <NoteItem note={note} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Notes;
