c70fdfe7eb5ef42c128bbc12688a4ae37485419f
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-20 01:21:12 +0500

    setting up fontawesome and using icons

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -8,6 +8,8 @@ const NoteItem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <i className="mx-2 fa-solid fa-trash-can"></i>
+          <i className="mx-2 fa-solid fa-pen-to-square"></i>
         </div>
       </div>
     </div>
--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -6,11 +6,13 @@ const Notes = () => {
   const { notes, setnotes } = context;
   return (
     <>
-      <div className="row my-3">
+      <div className="container my-3">
         <h1>Your Notes</h1>
-        {notes.map((note) => {
-          return <NoteItem note={note} />;
-        })}
+        <div className="row">
+          {notes.map((note) => {
+            return <NoteItem note={note} />;
+          })}
+        </div>
       </div>
     </>
   );
