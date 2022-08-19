75b16dbd3fc57c8665ed28b51917482fdfdacd9b
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-20 01:28:24 +0500

    some minor styling

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -6,10 +6,14 @@ const NoteItem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
+          <div className="d-flex align-items-right">
+            <h5 className="card-title">{note.title}</h5>{" "}
+            <div className="align-items-right">
+              <i className="mx-2 fa-solid fa-trash-can"></i>
+              <i className="mx-2 fa-solid fa-pen-to-square"></i>
+            </div>
+          </div>
           <p className="card-text">{note.description}</p>
-          <i className="mx-2 fa-solid fa-trash-can"></i>
-          <i className="mx-2 fa-solid fa-pen-to-square"></i>
         </div>
       </div>
     </div>
