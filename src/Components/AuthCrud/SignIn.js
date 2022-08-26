81d76e564d751cf637a0552a418de43ff8606a2e
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-26 23:38:01 +0500

    initial signIn functionality to get auth token

--- src/Components/AuthCrud/SignIn.js
+++ src/Components/AuthCrud/SignIn.js
@@ -1,42 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignIn = () => {
-  const handleSubmit = (e) => {
+  const [cred, setcred] = useState({
+    email: "",
+    password: "",
+  });
+  const onChange = (e) => {
+    setcred({ ...cred, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: cred.email, password: cred.password }),
+    });
+    const json = await response.json();
+    console.log(json);
   };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label for="email">Email address</label>
+          <label htmlFor="email">Email address</label>
           <input
             type="email"
             className="form-control"
             id="email"
             name="email"
+            value={cred.email}
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            onChange={onChange}
           />
           <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
           </small>
         </div>
         <div className="form-group">
-          <label for="password">Password</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             className="form-control"
             id="password"
+            value={cred.password}
             name="password"
             placeholder="Password"
+            onChange={onChange}
           />
         </div>
 
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onSubmit={handleSubmit}
-        >
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
